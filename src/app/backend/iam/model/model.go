4e4a1d7ff95fe266c0b47ac722f3c8b858571ffe
click2cloud-team5 <click2cloud-team5@click2cloud.net>
2022-01-20 11:19:46 +0530

    bug fix for username

--- src/app/backend/iam/model/model.go
+++ src/app/backend/iam/model/model.go
@@ -22,7 +22,7 @@ import (
 // User schema of the user table
 type User struct {
 	ID       int64  `json:"id"`
-	Username string `json:"username"`
+	Username string `json:"name"`
 	Password string `json:"password"`
 	Token    string `json:"token"`
 	Type     string `json:"type"`
