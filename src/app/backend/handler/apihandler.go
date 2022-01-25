74eef546182dedcd31cfd3acfc7092172fac0265
click2cloud-team5 <click2cloud-team5@click2cloud.net>
2022-01-25 15:51:18 +0530

    Added code for login error

--- src/app/backend/handler/apihandler.go
+++ src/app/backend/handler/apihandler.go
@@ -5108,12 +5108,15 @@ func (apiHandler *APIHandler) handleGetUser(w *restful.Request, r *restful.Respo
 		return
 	}
 
-	if user.ObjectMeta.Username != substrings[0] || user.ObjectMeta.Password != substrings[1] {
-		ErrMsg := ErrorMsg{Msg: "Invalid Username or Password"}
-		r.WriteHeaderAndEntity(http.StatusUnauthorized, ErrMsg)
-		return
+	//if user.ObjectMeta.Username != substrings[0] || user.ObjectMeta.Password != substrings[1] {
+	//ErrMsg := ErrorMsg{Msg: "Invalid Username or Password"}
+	//r.WriteHeaderAndEntity(http.StatusUnauthorized, ErrMsg)
+	//return
+	//}
+	{
+		r.WriteHeaderAndEntity(http.StatusOK, user)
 	}
-	r.WriteHeaderAndEntity(http.StatusOK, user)
+
 }
 
 type ErrorMsg struct {
